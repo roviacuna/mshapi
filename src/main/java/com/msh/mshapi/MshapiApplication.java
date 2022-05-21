a22308dcb3a0db163b34a02a5060e3e98a126d22
roviacuna <roviacuna@gmail.com>
2022-05-21 17:16:01 -0500

    base project

---
+++ src/main/java/com/msh/mshapi/MshapiApplication.java
@@ -0,0 +1,12 @@
+package com.msh.mshapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MshapiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MshapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msh/mshapi/MshapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msh.mshapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MshapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
